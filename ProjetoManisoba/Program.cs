efcfecbb81055a149002f4c4e4903c6878172043
Armando Filho <armandomonsaof@gmail.com>
2024-03-21 18:54:04 -0300

    Funcionalidades aplicadas

--- ProjetoManisoba/Program.cs
+++ ProjetoManisoba/Program.cs
@@ -9,15 +9,45 @@ namespace ProjetoManisoba
 {
     class Program
     {
+        public static void Copy(string sourceDirectory, string targetDirectory)
+        {
+            DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
+            DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
+
+            CopyAll(diSource, diTarget);
+        }
+
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            // Copy each file into the new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                    target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+
         public static void Main(string[] args)
         {
             try
             {
-                MprWrapper.WNetUseConnection(@"\\192.168.0.240", @"unibra.ub\armando", "trendkillbrutalkillpsychomurder");
+                //MprWrapper.WNetUseConnection(@"\\192.168.0.240", @"unibra.ub\armando", "trendkillbrutalkillpsychomurder");
+                MprWrapper.WNetUseConnection(@"\\192.168.0.229", @"unibra.ub\armando", "trendkillbrutalkillpsychomurder");
                 //Console.WriteLine("Conexão estabelecida com sucesso!");
 
-                string path = @"\\192.168.0.240\ti sistema\UB SMART PRD";
-                string[] files = System.IO.Directory.GetDirectories(path);
+                //string path = @"\\192.168.0.229\WKRadar\#2 RM SMART PRD";
+                string path = @"\\192.168.0.229\WKRadar\#2 RM SMART PRD\UB SMART PRD";
+                string[] files = System.IO.Directory.GetFiles(path);
                 string totvsDirectory = @"C:\TOTVStesste";
 
                 //Console.WriteLine("Arquivos na pasta específica:");
@@ -42,8 +72,9 @@ namespace ProjetoManisoba
                     Directory.CreateDirectory(totvsDirectory);
                 }
 
-                string fileToCopy = "\\\\192.168.0.229\\WKRadar\\#2 RM SMART PRD\\UB SMART PRD";
-                string destinationDirectory = $"C:\\{totvsDirectory}";
+                //string fileToCopy = @"\\192.168.0.229\WKRadar\#2 RM SMART PRD\UB SMART PRD 12.1.2402.116 (VERSAO ATT).zip";
+                string fileToCopy = @"\\192.168.0.229\WKRadar\#2 RM SMART PRD\UB SMART PRD";
+                string destinationDirectory = @$"{totvsDirectory}";
 
                 var di = new DirectoryInfo(fileToCopy);
 
@@ -56,9 +87,7 @@ namespace ProjetoManisoba
                     }
                 }
 
-                File.Copy(fileToCopy, destinationDirectory + Path.GetFileName(fileToCopy));
-
-                // + Path.GetFileName(fileToCopy)
+                Copy(fileToCopy, destinationDirectory);
             }
             catch (Exception ex)
             {
